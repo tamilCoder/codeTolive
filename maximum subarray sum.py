79ab076a9470be52414c3c55bb4ba06e1a8a5d75
tamilCoder <59533122+tamilCoder@users.noreply.github.com>
2020-05-13 16:17:42 +0530

    Create maximum subarray sum.py

---
+++ maximum subarray sum.py
@@ -0,0 +1,14 @@
+def maxSubArray(nums)
+        if not nums: //Checking the corner cases
+            return 0
+        currsum = maxsum = nums[0] //Initializing with 1st element
+        for i in nums[1:]: // running from 1 to length
+            if currsum > 0 : // checking if currsum is positive
+                currsum+=i
+            else:
+                currsum = i
+            maxsum=max(maxsum,currsum) // keeping track of maximum sum
+     
+        return maxsum
+        
+ maxSubArray([3,-3,2,0,-1,2,-4,2,1])
